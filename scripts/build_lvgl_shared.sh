6ac2796a6e47421b34cb5b8f5b63f9185f8c395e
Jason Kridner <jkridner@beagleboard.org>
2026-02-12 23:16:32 -0500

    9.4.0-20260212sdl

--- scripts/build_lvgl_shared.sh
+++ scripts/build_lvgl_shared.sh
@@ -19,7 +19,8 @@ clang \
   -DLV_CONF_SKIP=1 \
   -DLV_USE_TEST=1 \
   -DLV_USE_SNAPSHOT=1 \
+  -DLV_USE_SDL=1 \
   "${SOURCES[@]}" \
   -o "$OUTPUT_LIB"
 
-echo "Built $OUTPUT_LIB with LV_USE_TEST=1 and LV_USE_SNAPSHOT=1"
+echo "Built $OUTPUT_LIB with TEST, SNAPSHOT, and SDL support"
